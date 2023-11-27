5354f8a492a7a4f84b8c8c4ea8b729390338f589
Jesús Cova <jesuscova@jisparking.com>
2023-11-26 23:41:40 -0300

    Inicio

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/project/main.js'
+                'resources/js/main.js'
             ],
             refresh: true,
         }),
