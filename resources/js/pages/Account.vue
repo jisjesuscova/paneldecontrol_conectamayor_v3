f64dac95a818292782521b9cc38a66791feb4252
Jesús Cova <jesuscova@jisparking.com>
2024-03-10 21:49:02 -0300

    cambios

--- resources/js/pages/Account.vue
+++ resources/js/pages/Account.vue
@@ -16,6 +16,41 @@
 <script>
 export default {
   components: {
-  }
+  },
+  created() {
+    this.audit();
+  },
+  methods: {
+    async audit() {
+      const token = localStorage.getItem("token");
+
+      const id = localStorage.getItem("id");
+      
+      if (token) {
+        const formData = new FormData();
+
+        formData.append("user_id", id);
+        formData.append("task_id", '0');
+        formData.append("task", 'Página Principal');
+
+        try {
+          const response = await axios.post(
+                  "https://paneldecontrolem.cl/api/audit/store",
+                  formData,
+                  {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                      "Content-Type": "multipart/form-data",
+                    },
+                  }
+                );
+        } catch (error) {
+          console.error("Error al guardar la auditoría:", error);
+        }
+      } else {
+        this.$router.push("/login");
+      }
+    },
+  },
 }
 </script>
